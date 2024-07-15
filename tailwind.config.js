2640daad6b6afc8e97c32b227e8c94fb45930341
unknown <amir>
2024-07-15 20:11:48 +0330

    version-1-golestan-tea

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.{html,js}"],
+  darkMode:"class",
+  theme: {
+    extend: {},
+    screens:{
+      "mobile":"340px",
+      "desktop":"1000px"
+    }
+  },
+  plugins: [],
+}
+
